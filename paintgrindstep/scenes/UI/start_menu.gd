6cc8b96cb3ff7705e38069a6d3201079a4c3f1e7
A-somenumbers <79110913+A-somenumbers@users.noreply.github.com>
2025-07-14 14:17:25 -0400

    Working on a hub world

--- paintgrindstep/scenes/UI/start_menu.gd
+++ paintgrindstep/scenes/UI/start_menu.gd
@@ -5,7 +5,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/hub_world.tscn")
 
 
 func _on_quit_pressed() -> void:
